e955b9c5222c5a967e0dfd5e216679b8e2450db6
Shreyansh Agarwal <shreyansh@aus.co.in>
2022-08-24 00:29:08 +0530

    Fix movie not found crashing bug.

--- utils/api/rt.py
+++ utils/api/rt.py
@@ -3,8 +3,8 @@ from rotten_tomatoes_scraper.rt_scraper import MovieScraper
 
 
 def fetch_movie_data_from_rt(movie_title: str):
-    movie_scrapper = MovieScraper(movie_title=movie_title)
     try:
+        movie_scrapper = MovieScraper(movie_title=movie_title)
         movie_scrapper.extract_metadata()
         rating = pydash.get(movie_scrapper.metadata, "Score_Rotten", 0.0)
         return float(rating) / 10
